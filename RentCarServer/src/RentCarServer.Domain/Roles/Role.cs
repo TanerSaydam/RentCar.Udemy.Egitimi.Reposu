5970a1564769d731c87dcad12af2c7ded352aad1
Taner Saydam <taner_and_you@hotmail.com>
2025-07-05 17:25:46 +0300

    Role modeline permission yapısı ekleyip permission update metodu yazalım

--- RentCarServer/src/RentCarServer.Domain/Roles/Role.cs
+++ RentCarServer/src/RentCarServer.Domain/Roles/Role.cs
@@ -2,8 +2,9 @@
 using RentCarServer.Domain.Branchs.ValueObjects;
 
 namespace RentCarServer.Domain.Roles;
-public sealed class Role : Entity
+public sealed class Role : Entity, IAggregate
 {
+    private readonly List<Permission> _permissions = new();
     private Role() { }
 
     public Role(Name name, bool isActive)
@@ -12,11 +13,20 @@ public sealed class Role : Entity
         SetStatus(IsActive);
     }
     public Name Name { get; private set; } = default!;
+    public IReadOnlyCollection<Permission> Permissions => _permissions;
 
     #region Behaviors
     public void SetName(Name name)
     {
         Name = name;
     }
+
+    public void SetPermissions(IEnumerable<Permission> permissions)
+    {
+        _permissions.Clear();
+        _permissions.AddRange(permissions);
+    }
     #endregion
 }
+
+public sealed record Permission(string Value);
