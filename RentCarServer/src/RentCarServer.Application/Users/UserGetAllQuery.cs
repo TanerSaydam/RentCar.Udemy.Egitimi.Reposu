f74d217ca842d07fe282bcbe9364df7d127fad06
Taner Saydam <taner_and_you@hotmail.com>
2025-07-06 08:21:19 +0300

    User listesi role göre belirlensin

--- RentCarServer/src/RentCarServer.Application/Users/UserGetAllQuery.cs
+++ RentCarServer/src/RentCarServer.Application/Users/UserGetAllQuery.cs
@@ -1,4 +1,5 @@
 ﻿using RentCarServer.Application.Behaviors;
+using RentCarServer.Application.Services;
 using RentCarServer.Domain.Branches;
 using RentCarServer.Domain.Roles;
 using RentCarServer.Domain.Users;
@@ -11,12 +12,21 @@ public sealed record UserGetAllQuery : IRequest<IQueryable<UserDto>>;
 internal sealed class UserGetAllQueryHandler(
     IUserRepository userRepository,
     IRoleRepository roleRepository,
+    IClaimContext claimContext,
     IBranchRepository branchRepository) : IRequestHandler<UserGetAllQuery, IQueryable<UserDto>>
 {
     public Task<IQueryable<UserDto>> Handle(UserGetAllQuery request, CancellationToken cancellationToken)
-        => Task.FromResult(
-            userRepository
+    {
+        var res = userRepository
             .GetAllWithAudit()
             .MapTo(roleRepository
-            .GetAll(), branchRepository.GetAll()));
+            .GetAll(), branchRepository.GetAll());
+
+        if (claimContext.GetRoleName() != "sys_admin")
+        {
+            res = res.Where(i => i.BranchId == claimContext.GetBranchId());
+        }
+
+        return Task.FromResult(res);
+    }
 }
